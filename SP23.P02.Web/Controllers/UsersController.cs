90a1f10313e550c6aa402eb6f45f7206cd6b0d27
Zoe Klein <zoe.klein@selu.edu>
2023-02-13 18:14:22 -0600

    wip; set up UsersController before adding methods

---
+++ SP23.P02.Web/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SP23.P02.Web.Data;
+using SP23.P02.Web.Features.TrainStations;
+using SP23.P02.Web.Features.Users;
+
+namespace SP23.P02.Web.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DbSet<User> users;
+        private readonly DataContext dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+            users = dataContext.Set<User>();
+        }//end UsersController(DataContext)
+
+    }//end UsersController
+}//end namespace
